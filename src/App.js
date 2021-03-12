e2116774682df7fe44cc43ce66811c43ceee346a
Diego Ruiz <diego25061@gmail.com>
2021-03-12 12:34:29 -0500

    Added semantic ui theme, prettier.rc configs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import CommitsList from './components/CommitsList'
+import { List, Container, Header, Segment } from 'semantic-ui-react'
 
 function App() {
-  return (
+    return (
+        
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Container>
+            <Header as="h2" textAlign="center">Commits List</Header>
+            <Segment padded="very">
+                <CommitsList />
+            </Segment>
+        </Container>
     </div>
-  );
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/CommitsList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { List, Container, Header, Segment } from 'semantic-ui-react'
+
+export default class CommitsList extends React.Component {
+    state = {}
+
+    render = () => {
+        return (
+            <div></div>
+        )
+    }
+}
